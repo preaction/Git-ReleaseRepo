d38146480697748fa617276e1553c008f6e554f6
Doug Bell <madcityzen@gmail.com>
2012-11-07 20:38:22 -0600

    add an accessor for the release repository

--- lib/Git/ReleaseRepo/Command.pm
+++ lib/Git/ReleaseRepo/Command.pm
@@ -7,6 +7,7 @@ use App::Cmd::Setup -command;
 use YAML::Tiny;
 use File::HomeDir;
 use File::Spec::Functions qw( catfile );
+use Git::Repository;
 
 has config_file => (
     is      => 'ro',
@@ -35,6 +36,20 @@ sub write_config {
     my ( $self ) = @_;
     return $self->config->write( $self->config_file );
 }
+
+has git => (
+    is      => 'ro',
+    isa     => 'Git::Repository',
+    lazy    => 1,
+    default => sub {
+        my ( $self ) = @_;
+        my $repo = $self->config->[0];
+        my $repo_name = [keys %$repo]->[0];
+        return Git::Repository->new(
+            work_tree => $repo->{$repo_name}{work_tree},
+        );
+    },
+);
 
 sub execute {
     my ( $self, $opt, $args ) = @_;
