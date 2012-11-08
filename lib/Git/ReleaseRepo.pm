31571693b3e889215ff06aff51d61721ba55f5e2
Doug Bell <madcityzen@gmail.com>
2012-11-07 19:18:36 -0600

    start with a clone method and a config file

---
+++ lib/Git/ReleaseRepo.pm
@@ -0,0 +1,17 @@
+package Git::ReleaseRepo;
+
+use strict;
+use warnings;
+use App::Cmd::Setup -app;
+
+
+1;
+__END__
+
+=head1 NAME
+
+Git::ReleaseRepo - Manage a release repository of submodules
+
+=head1 DESCRIPTION
+
+
---
+++ lib/Git/ReleaseRepo/Command.pm
@@ -0,0 +1,48 @@
+package Git::ReleaseRepo::Command;
+
+use strict;
+use warnings;
+use Moose;
+use App::Cmd::Setup -command;
+use YAML::Tiny;
+use File::HomeDir;
+use File::Spec::Functions qw( catfile );
+
+has config_file => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => sub {
+        catfile( File::HomeDir->my_home, '.releaserepo' );
+    },
+);
+
+has config => (
+    is      => 'ro',
+    isa     => 'YAML::Tiny',
+    default => sub {
+        my ( $self ) = @_;
+        if ( -f $self->config_file ) {
+            return YAML::Tiny->read( $self->config_file );
+        }
+        else {
+            return YAML::Tiny->new;
+        }
+    },
+);
+
+sub write_config {
+    my ( $self ) = @_;
+    return $self->config->write( $self->config_file );
+}
+
+sub execute {
+    my ( $self, $opt, $args ) = @_;
+    inner();
+    $self->write_config;
+}
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
+__END__
