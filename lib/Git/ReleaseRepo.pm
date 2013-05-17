a3b267cf0deed719865ccec3ee53a169e75c6cc7
Doug Bell <doug.bell@baml.com>
2013-05-17 17:38:10 -0400

    add some overview docs about tags, branches, and semantic versioning

--- lib/Git/ReleaseRepo.pm
+++ lib/Git/ReleaseRepo.pm
@@ -14,4 +14,28 @@ Git::ReleaseRepo - Manage a release repository of submodules
 
 =head1 DESCRIPTION
 
+This application manages a Git repository for releases. It also follows the
+"Semantic Versioning" specification.
+
+=head1 SUBMODULES
+
+=head1 BRANCHES AND TAGS
+
+Branches are for major and minor releases. Tags are for bugfix releases.
+
+    v1.0 - Branch for the 1.0 release cycle
+    v1.0.0 - Tag for the first release in the 1.0 release cycle
+    v1.0.1 - Tag for a bugfix release
+    v1.1 - Branch for the 1.1 release cycle
+    v1.1.0 - Tag for the first release in the 1.1 release cycle
+
+The tip of the release branch will always be the latest code for that release,
+even if it is not yet part of a bugfix release.
+
+Branches will only ever be m/^${PREFIX}\d+[.]\d+/.
+
+=head1 SEMANTIC VERSIONING
+
+See also: http://semver.org/
+
 
