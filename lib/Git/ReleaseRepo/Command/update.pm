a60cc921c488b5b1bd53024327ccbf576045375a
Doug Bell <doug.bell@baml.com>
2013-06-03 18:43:14 -0400

    add update command to update a deploy repository

---
+++ lib/Git/ReleaseRepo/Command/update.pm
@@ -0,0 +1,54 @@
+package Git::ReleaseRepo::Command::update;
+# ABSTRACT: Update a deployed release repository
+
+use Moose;
+extends 'Git::ReleaseRepo::Command';
+
+override usage_desc => sub {
+    my ( $self ) = @_;
+    return super();
+};
+
+sub description {
+    return 'Update a deployed release repository';
+}
+
+sub validate_args {
+    my ( $self, $opt, $args ) = @_;
+    return $self->usage_error( "Too many arguments" ) if ( @$args > 0 );
+}
+
+around opt_spec => sub {
+    my ( $orig, $self ) = @_;
+    return (
+        $self->$orig,
+        [ 'branch=s' => 'Specify the release branch to deploy. Defaults to the latest release branch.' ],
+        [ 'master' => 'Deploy the "master" version of the repository and all submodules, for testing.' ],
+    );
+};
+
+augment execute => sub {
+    my ( $self, $opt, $args ) = @_;
+    my $repo_name   = $self->repo_name;
+    my $repo        = $self->git;
+    my $branch      = $opt->{master} ? "master"
+                    : $opt->{branch} ? $opt->{branch}
+                    : $self->config->{ $repo_name }{track};
+    my $version     = $opt->{master}  ? "master"
+                    : $repo->latest_version( $branch );
+    $repo->checkout( $version );
+    if ( $opt->{master} ) {
+        my $cmd = $repo->command( submodule => 'foreach', 'git checkout master && git pull origin master' );
+        my @stderr = readline $cmd->stderr;
+        my @stdout = readline $cmd->stdout;
+        $cmd->close;
+        if ( $cmd->exit != 0 ) {
+            die "Could not checkout master\nEXIT: " . $cmd->exit . "\nSTDERR: " . ( join "\n", @stderr )
+                . "\nSTDOUT: " . ( join "\n", @stdout );
+        }
+    }
+};
+
+1;
+__END__
+
